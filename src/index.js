a8cbe52365b0b20f3811a1664ac1b4cc1da7b2cb
tspenov <tsvetozar.penov@airhelp.com>
2017-09-14 16:13:57 +0300

    Initial version of Todo app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,95 @@
 import { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Hello World!</h2>
-            </div>
-        )
-    }
+const todosReducer = (state = {todos: []}, action) => {
+	switch(action.type) {
+		case 'ADD_TODO':
+			return Object.assign(state, {
+				todos: [
+					...state.todos,
+					{
+					// id: action.id,
+						 text: action.text,
+					// completed: false
+					}
+				]})
+		default:
+			return state
+	}
 }
 
-ReactDOM.render( <App />, document.querySelector( '#app' ) );
+const store = createStore(todosReducer);
+
+class TodoApp extends React.Component {
+
+	render() {
+		return(
+			<div>
+		 		<TodoHeader />
+		 		<AddTodo />
+				<TodoList todos={this.props.todos}/>
+   		</div>
+		);
+	}
+};
+
+const TodoHeader = () => (
+  <h2>
+    Todo App Header
+  </h2>
+);
+
+class AddTodo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {value: ''};
+	}
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+
+	handleClick(event) {
+		if (this.state.value != '') {
+			console.log(this.state.value)
+			store.dispatch({
+				type: 'ADD_TODO',
+				text: this.state.value,
+			});
+			console.log(store.getState())
+			// this.props.onAddTodo({text: this.state.value});
+			this.setState(prevState => ({value: ''}));
+		}
+	}
+
+	render () {
+		return (
+			<div>
+  			<input type="text" value={this.state.value} onChange={this.handleChange.bind(this)}/>
+				<button onClick={this.handleClick.bind(this)}>
+					Add todo
+				</button>
+		</div>
+	 );
+	}
+};
+
+const TodoList = (props) => (
+	<ul>
+		{
+			props.todos.map((todo) => (<li>{todo.text}</li>))
+		}
+	</ul>
+);
+
+//const TodoFooter = () => (
+//);
+
+
+const render = () => {
+  ReactDOM.render(<TodoApp todos={store.getState().todos}/>, document.getElementById("root"))
+};
+
+store.subscribe(render);
+render();
